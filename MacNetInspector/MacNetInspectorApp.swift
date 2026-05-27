0ccbff2dcfb769c4a5d5066e1643c35ac43ef383
HUNTER <HUNTER>
2026-05-28 00:10:36 +0300

    Initial Commit

---
+++ MacNetInspector/MacNetInspectorApp.swift
@@ -0,0 +1,17 @@
+//
+//  MacNetInspectorApp.swift
+//  MacNetInspector
+//
+//  Created by Павел Волкинд on 5/28/26.
+//
+
+import SwiftUI
+
+@main
+struct MacNetInspectorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MacNetInspectorTests/MacNetInspectorTests.swift
@@ -0,0 +1,19 @@
+//
+//  MacNetInspectorTests.swift
+//  MacNetInspectorTests
+//
+//  Created by Павел Волкинд on 5/28/26.
+//
+
+import Testing
+@testable import MacNetInspector
+
+struct MacNetInspectorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
